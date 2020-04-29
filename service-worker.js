96c234f65cb0c6b4a1736f04cd08e0812f221004
Ali Sherief <alihsherief@linuxmail.org>
2020-04-29 23:17:14 +0200

    save files

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.873697cb1ea61dff21a62fce0c9c69b9.js"
+  "/precache-manifest.b7972f92fc7ccec8973b6793f3fbdb43.js"
 );
 
 self.addEventListener('message', (event) => {
